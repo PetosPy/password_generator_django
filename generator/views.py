579a712feebb56759d03aff983a3d3229f1dbbc7
Michael <haraebm@gmail.com>
2021-10-19 01:36:09 -0300

    First commit

---
+++ generator/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import random
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'generator/home.html')
+
+def about(request):
+    return render(request, 'generator/about.html')
+
+
+def password(request):
+
+    special_chars = list("@#%$&*{?]}(-")
+    numbers = list("1234567890")
+    uppercase = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    characters = list("abcdefghijklmnopqrstuvwxyz")
+
+    
+    if request.GET.get("uppercase"):
+        characters.extend(uppercase)
+
+    if request.GET.get("special_characters"):
+        characters.extend(special_chars)
+
+    if request.GET.get("numbers"):
+        characters.extend(numbers)
+
+    length = int(request.GET.get("length", "12"))
+
+    the_password = ""
+
+    for x in range(length):
+        the_password += random.choice(characters)
+
+
+
+    return render(request, 'generator/password.html', {"password": the_password})
+
